738956763eb90374b63510e5499810fd73dbdde2
Lengyel Norbert <lengyelnorbert@hotmail.com>
2017-06-14 17:39:00 +0200

    enum classes

--- src/main/java/com/greenfox/poker/model/Deck.java
+++ src/main/java/com/greenfox/poker/model/Deck.java
@@ -1,6 +1,17 @@
 package com.greenfox.poker.model;
 
 
+import java.util.List;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 public class Deck {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private long id;
+
+  private List<Card> cards;
 
 }
--- src/main/java/com/greenfox/poker/model/Round.java
+++ src/main/java/com/greenfox/poker/model/Round.java
@@ -4,10 +4,17 @@ package com.greenfox.poker.model;
 public enum Round {
   BETTING("Betting"), FLOP("Flop"), TURN("Turn"), RIVER("River"), SHOWDOWN("Showdown"), WAITING("Waiting");
 
-  private final String name;
+  private final String round;
 
   private Round(String s) {
-    name = s;
+    round = s;
+  }
+
+  @Override
+  public String toString() {
+    return "Round{" +
+            "roundName='" + round + '\'' +
+            '}';
   }
 }
 
