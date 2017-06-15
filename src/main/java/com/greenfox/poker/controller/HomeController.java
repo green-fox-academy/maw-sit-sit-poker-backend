725a4daee01536d481d719c9edb00afeae922c14
Gabor Kiszler <gabor.kiszler@gmail.com>
2017-06-15 11:59:15 +0200

    clean HomeController

--- src/main/java/com/greenfox/poker/controller/HomeController.java
+++ src/main/java/com/greenfox/poker/controller/HomeController.java
@@ -1,12 +1,5 @@
 package com.greenfox.poker.controller;
 
-import com.greenfox.poker.model.Card;
-import com.greenfox.poker.model.PokerUser;
-import com.greenfox.poker.model.Rank;
-import com.greenfox.poker.model.Suit;
-import com.greenfox.poker.repository.PokerUserRepo;
-import com.greenfox.poker.service.GamePlayerService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -14,19 +7,9 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class HomeController {
 
-  @Autowired
-  PokerUserRepo pokerUserRepo;
-
-  @Autowired
-  GamePlayerService gamePlayerService;
-
   @GetMapping("/")
   @ResponseBody
   public String home() {
-    pokerUserRepo.save(new PokerUser("James Bond", "VESPER", "james@bond.uk", "www.007.com"));
-    pokerUserRepo.save(new PokerUser("Ramin James Shamsi Bond", "123456VESPER",
-            "RaminJames@ShamsiBond.uk", "www.006007.com"));
-    gamePlayerService.getGamePlayer(1234);
-    return "Hello";
+    return "hello";
   }
 }
--- src/main/java/com/greenfox/poker/repository/PokerUserRepo.java
+++ src/main/java/com/greenfox/poker/repository/PokerUserRepo.java
@@ -4,7 +4,9 @@ package com.greenfox.poker.repository;
 import com.greenfox.poker.model.PokerUser;
 import java.util.List;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Component;
 
+@Component
 public interface PokerUserRepo extends CrudRepository<PokerUser, Long> {
   List<PokerUser> findAll();
 }
