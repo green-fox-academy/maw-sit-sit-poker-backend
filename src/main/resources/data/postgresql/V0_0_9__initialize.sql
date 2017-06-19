555612ab8c2b0cd61f89e4818819e031ba058ca7
Lengyel Norbert <lengyelnorbert@hotmail.com>
2017-06-19 10:50:15 +0200

    database fixed again

--- src/main/resources/data/postgresql/V0_0_9__initialize.sql
+++ src/main/resources/data/postgresql/V0_0_9__initialize.sql
@@ -1,18 +1,18 @@
 DROP TABLE IF EXISTS game;
 CREATE TABLE game (
-  id                  SERIAL,
-  name                VARCHAR(255) NOT NULL,
-  big_blind           INTEGER      NOT NULL,
-  max_players_num     INTEGER      NOT NULL,
-  current_players_num INTEGER,
+  id              SERIAL,
+  name            VARCHAR(255) NOT NULL,
+  big_blind       INTEGER      NOT NULL,
+  max_players     INTEGER      NOT NULL,
+  current_players INTEGER,
   PRIMARY KEY (id)
 );
 
-INSERT INTO game (id, name, big_blind, max_players_num, current_players_num)
+INSERT INTO game (id, name, big_blind, max_players, current_players)
 VALUES ('1', 'BigBoss Ramins favourite table', '500', '6', '2');
 
-INSERT INTO game (id, name, big_blind, max_players_num, current_players_num)
+INSERT INTO game (id, name, big_blind, max_players, current_players)
 VALUES ('2', 'Lucky Gabors winning table', '200', '4', NULL);
 
-INSERT INTO game (id, name, big_blind, max_players_num, current_players_num)
+INSERT INTO game (id, name, big_blind, max_players, current_players)
 VALUES ('3', 'Norberts table', '100', '2', '1');
