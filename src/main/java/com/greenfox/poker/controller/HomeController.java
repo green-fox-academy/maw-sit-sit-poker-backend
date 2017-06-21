a9030c4ad51b6903ab1d7fc07cf00f6c925cc5af
Lengyel Norbert <lengyelnorbert@hotmail.com>
2017-06-21 16:09:24 +0200

    deck methods added

--- src/main/java/com/greenfox/poker/controller/HomeController.java
+++ src/main/java/com/greenfox/poker/controller/HomeController.java
@@ -1,5 +1,7 @@
 package com.greenfox.poker.controller;
 
+import com.greenfox.poker.model.Deck;
+import com.greenfox.poker.service.DeckService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -10,6 +12,12 @@ public class HomeController {
   @GetMapping("/")
   @ResponseBody
   public String home() {
+
+    DeckService deckService = new DeckService();
+    Deck newDeckToTestHere = deckService.getNewDeck();
+    System.out.println(newDeckToTestHere.toString());
+    deckService.shuffleDeck(newDeckToTestHere);
+    System.out.println(newDeckToTestHere.toString());
     return "Hello";
   }
 }
