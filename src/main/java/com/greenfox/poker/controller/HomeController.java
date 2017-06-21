03a611294a3f9d5f9544f9b27afc1f3069654508
Gabor Kiszler <gabor.kiszler@gmail.com>
2017-06-21 10:33:24 +0200

    add crossorigin annotation to controllers

--- src/main/java/com/greenfox/poker/controller/HomeController.java
+++ src/main/java/com/greenfox/poker/controller/HomeController.java
@@ -1,9 +1,11 @@
 package com.greenfox.poker.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+@CrossOrigin("*")
 @Controller
 public class HomeController {
 
