a5bd2607fb470ba6ceac38ae6e4fbf21507e3af2
Gabor Kiszler <gabor.kiszler@gmail.com>
2017-06-22 14:36:34 +0200

    GameState refactored, now stored in HashMap

--- src/main/java/com/greenfox/poker/service/GameService.java
+++ src/main/java/com/greenfox/poker/service/GameService.java
@@ -4,9 +4,9 @@ import com.greenfox.poker.model.Game;
 import com.greenfox.poker.model.GamePlayer;
 import com.greenfox.poker.model.GameState;
 import com.greenfox.poker.model.PokerUserDTO;
-import com.greenfox.poker.model.ResponseType;
 import com.greenfox.poker.repository.GameRepo;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -20,7 +20,7 @@ public class GameService {
   @Autowired
   UserService userService;
 
-  List<GameState> gameStateList = new ArrayList<>();
+  HashMap<Long, GameState> gameStateMap = new HashMap<>();
 
   public Game saveGame(Game game){
     gameRepo.save(game);
@@ -43,28 +43,20 @@ public class GameService {
   }
 
   public GameState getGameState(long id) {
-    for (GameState gameState : gameStateList) {
-      if (id == gameState.getId()) {
-        return gameState;
-      }
-    }
-    return new GameState();
+    return gameStateMap.get(id);
   }
 
   public List<PokerUserDTO> getUserDTOListFromGame(Game game){
     long thisGameStateId = game.getGamestate_id();
     List<PokerUserDTO> currentPlayersInTheGame = new ArrayList<>();
     List<GamePlayer> gamePlayers = new ArrayList<>();
-    for (GameState state : gameStateList) {
-      if (state.getId() == thisGameStateId){
-       gamePlayers = state.getPlayers();
-      }
-    }
+    gamePlayers = gameStateMap.get(thisGameStateId).getPlayers();
     for (GamePlayer player : gamePlayers){
       currentPlayersInTheGame.add(player.getPlayer());
     }
     return currentPlayersInTheGame;
   }
+
 
   public boolean isPlayerAlreadyInTheGame(long gameId, long userId){
     for (PokerUserDTO userDto : getUserDTOListFromGame(gameRepo.findOne(gameId))){
@@ -75,15 +67,9 @@ public class GameService {
     return false;
   }
 
-  public void updateGameStateList(GameState gameState, int indexOfGameStateInList){
-    gameStateList.set(indexOfGameStateInList, gameState);
-  }
-
   public void joinPlayerToGame(PokerUserDTO pokerUserDTO, long gameId){
     GamePlayer newPlayer = new GamePlayer(pokerUserDTO);
-    GameState currentGameState = getGameState(gameRepo.findOne(gameId).getGamestate_id());
-    int index = gameStateList.indexOf(currentGameState);
-    currentGameState.getPlayers().add(newPlayer);
-    updateGameStateList(currentGameState, index);
+    long stateId = gameRepo.findOne(gameId).getGamestate_id();
+    getGameState(stateId).getPlayers().add(newPlayer);
   }
 }
