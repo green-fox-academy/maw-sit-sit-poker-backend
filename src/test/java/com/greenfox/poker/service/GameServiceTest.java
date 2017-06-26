61a327453bbe7eac2e572220202a08d6eebde33c
Lengyel Norbert <lengyelnorbert@hotmail.com>
2017-06-26 12:13:13 +0200

    test methods checked

--- src/test/java/com/greenfox/poker/service/GameServiceTest.java
+++ src/test/java/com/greenfox/poker/service/GameServiceTest.java
@@ -63,14 +63,14 @@ public class GameServiceTest {
     assertEquals(1, 1);
   }
 
-//  @Test
-//  public void testGetGames() throws Exception {
-//    this.mockMvc.perform(get("/games")
-//            .contentType(contentType))
-//            .andExpect(jsonPath("$.[0].id").exists())
-//            .andExpect(jsonPath("$.[0].name").exists())
-//            .andExpect(jsonPath("$.[0].big_blind").exists())
-//            .andExpect(jsonPath("$.[0].max_players").exists())
-//            .andExpect(jsonPath("$.[0].gamestate_id").exists());
-//  }
+  @Test
+  public void testGetGames() throws Exception {
+    this.mockMvc.perform(get("/games")
+            .contentType(contentType))
+            .andExpect(jsonPath("$.[0].id").exists())
+            .andExpect(jsonPath("$.[0].name").exists())
+            .andExpect(jsonPath("$.[0].big_blind").exists())
+            .andExpect(jsonPath("$.[0].max_players").exists())
+            .andExpect(jsonPath("$.[0].gamestate_id").exists());
+  }
 }
