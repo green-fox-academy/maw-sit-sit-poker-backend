f7def10df51d44f924cefdb9daea4a6ec8edc8ea
Ramin <RaminShamsi@yahoo.com>
2017-07-14 10:34:22 +0200

    initail commit of my own branch

--- src/test/java/com/greenfox/poker/service/DeckServiceTest.java
+++ src/test/java/com/greenfox/poker/service/DeckServiceTest.java
@@ -26,11 +26,11 @@ public class DeckServiceTest {
     testDeck = deckService.getNewDeck();
     cardsInTestDeck = testDeck.getCards();
     validCardList = Arrays
-            .asList("C2", "D2", "H2", "S2", "C3", "D3", "H3", "S3", "C4", "D4", "H4", "S4", "C5",
-                    "D5", "H5", "S5", "C6", "D6", "H6", "S6", "C7", "D7", "H7", "S7", "C8", "D8",
-                    "H8", "S8", "C9", "D9", "H9", "S9", "CT", "DT", "HT", "ST", "CJ", "DJ", "HJ",
-                    "SJ", "CD", "DD", "HD", "SD", "CK", "DK", "HK", "SK", "CA", "DA", "HA", "SA"
-            );
+        .asList("C2", "D2", "H2", "S2", "C3", "D3", "H3", "S3", "C4", "D4", "H4", "S4", "C5",
+            "D5", "H5", "S5", "C6", "D6", "H6", "S6", "C7", "D7", "H7", "S7", "C8", "D8",
+            "H8", "S8", "C9", "D9", "H9", "S9", "CT", "DT", "HT", "ST", "CJ", "DJ", "HJ",
+            "SJ", "CD", "DD", "HD", "SD", "CK", "DK", "HK", "SK", "CA", "DA", "HA", "SA"
+        );
   }
 
   @After
