e54d33db73497ffec870a9994710628895f2cb8a
Ramin <RaminShamsi@yahoo.com>
2017-07-17 12:36:13 +0200

    made choose5CardsOutOf7 method with input

--- src/main/java/com/greenfox/poker/controller/HomeController.java
+++ src/main/java/com/greenfox/poker/controller/HomeController.java
@@ -1,8 +1,14 @@
 package com.greenfox.poker.controller;
 
+import com.greenfox.poker.model.Card;
+import com.greenfox.poker.model.Deck;
 import com.greenfox.poker.service.Accessible;
 
+import com.greenfox.poker.service.DeckService;
 import com.greenfox.poker.service.ShowDownService;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -20,7 +26,12 @@ public class HomeController {
   @ResponseBody
   @Accessible
   public String home() {
-    showDownService.choose5CardsOutOf7();
+    DeckService deckService = new DeckService();
+    Deck myDeck = deckService.getNewDeck();
+    deckService.shuffleDeck(myDeck);
+    List<Card> playerCards = new ArrayList<>(Arrays.asList(deckService.drawCardFromDeck(myDeck),
+        deckService.drawCardFromDeck(myDeck)));
+    showDownService.choose5CardsOutOf7(playerCards);
     return "Hello";
   }
 }
--- src/main/java/com/greenfox/poker/service/ShowDownService.java
+++ src/main/java/com/greenfox/poker/service/ShowDownService.java
@@ -20,10 +20,11 @@ public class ShowDownService {
   @Autowired
   GameState gameState;
 
+  private static List<Card[]> chosen5Cards = new ArrayList<>();
 
-  private List<Card> all7cards() {
+  private List<Card> all7cards(List<Card> playerCards) {
     List<Card> all7Cards = new ArrayList<>();
-    //   all7Cards.addAll(gamePlayerHand.getPlayerCards());
+       all7Cards.addAll(playerCards);
     //   all7Cards.addAll(gameState.getCardsOnTable());
     DeckService deckService = new DeckService();
     Deck myDeck = deckService.getNewDeck();
@@ -33,41 +34,29 @@ public class ShowDownService {
         deckService.drawCardFromDeck(myDeck),
         deckService.drawCardFromDeck(myDeck),
         deckService.drawCardFromDeck(myDeck))));
-    deckService.shuffleDeck(myDeck);
-    all7Cards.addAll(new ArrayList<>(Arrays.asList(deckService.drawCardFromDeck(myDeck),
-        deckService.drawCardFromDeck(myDeck))));
+
     return all7Cards;
   }
 
-  public List<Card> choose5CardsOutOf7() {
-    List<Card> allSevenCards = all7cards();
+  public List<Card[]> choose5CardsOutOf7(List<Card> playerCards) {
+    List<Card> allSevenCards = all7cards(playerCards);
     Card[] sevenCard = new Card[7];
     for (int i = 0; i < sevenCard.length; i++) {
       sevenCard[i] = allSevenCards.get(i);
-      System.out.println(allSevenCards.get(i));
-      System.out.println("###############");
-      System.out.println(sevenCard[i]);
     }
     Card[] chosenCardsArray = new Card[5];
     combinationsOf5(sevenCard, 5, 0, chosenCardsArray);
-    System.out.println("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXxx");
-    for (Card[] item : resultList) {
-  //    System.out.println(item[0] + " " + item[1] + " " + item[2] + " " + item[3] + " " + item[4]);
+    for (Card[] item : chosen5Cards) {
+      //    System.out.println(item[0] + " " + item[1] + " " + item[2] + " " + item[3] + " " + item[4]);
       System.out.println(Arrays.toString(item));
-
     }
-    List<Card> chosenCards = new ArrayList<>();
-
-    System.out.println(resultList.size());
-    return chosenCards;
+    System.out.println(chosen5Cards.size());
+    return chosen5Cards;
   }
-
-  private static List<Card[]> resultList = new ArrayList<>();
 
   private void combinationsOf5(Card[] input, int len, int startPosition, Card[] result) {
     if (len == 0) {
-      resultList.add(Arrays.copyOf(result,5));
-      System.out.println(Arrays.toString(result));
+      chosen5Cards.add(Arrays.copyOf(result, 5));
       return;
     }
     for (int i = startPosition; i <= input.length - len; i++) {
@@ -75,6 +64,8 @@ public class ShowDownService {
       combinationsOf5(input, len - 1, i + 1, result);
     }
   }
+
+
 }
 
 
